d03026897f9ee4e1c96c718c0e46efd3291c4c3b
Yuki Kaneko <yukikaneko0730@icloud.com>
2025-11-12 11:18:18 +0100

    Revert "Feature/server setup"

--- server/libs/helpers.ts
+++
@@ -1,5 +0,0 @@
-import type { ErrorType } from './types'
-
-export function createError(status: number, message: string): ErrorType {
-     return { status, message };
-}
--- server/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController';
-
-const user = express.Router();
-
-user.post('/login', loginUser);
-user.post('/register', registerUser);
-
-export default user;
