5b6bc2816021b487a5ad3076882f35413e8f014e
Yukita0730 <yuki.haertinger@dci-student.org>
2025-11-12 17:18:00 +0100

    navbar and css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
+import Navbar from '../components/Navbar'
 
-import './App.css'
+export default function App() {
+  const user = {
+    name: 'Marie',
+    photoURL: 'https://example.com/avatar.jpg',
+  }
 
-function App() {
-  
+  return (
+    <div className="flex">
+      {/* ← 左サイドバー */}
+      <Navbar user={user} />
 
-  
-   
+      {/* ← メインエリア */}
+      <main className="flex-1 bg-[#f8f8f8] min-h-screen p-8">
+        <h1 className="text-2xl font-bold">Welcome to Coffeemates ☕️</h1>
+      </main>
+    </div>
+  )
 }
-
-export default App
