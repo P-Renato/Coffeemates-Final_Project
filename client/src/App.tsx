f67fcb91e1559e0cca9c0dbac5761e9a7f19395c
Renato <paulo.borupdebakker@dci-student.org>
2025-11-17 15:32:49 +0100

    Update for login page with API endpoints to the backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
 import MainLayout from "./layouts/MainLayout";
-
+import { AuthProvider } from './context/AuthProvider';
+import ProtectedRoute from "./components/ProtectedRoute";
 import RegisterPage from "./features/auth/RegisterPage";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./features/search/SearchPage";
@@ -10,30 +11,44 @@ import ProfilePage from "./features/profile/ProfilePage";
 import SettingPage from "./features/settings/SettingPage";
 
 const LoginPage = () => <div>Login Page Coming Soon!</div>;
+// const LoginPage = () => <div>Login Page Coming Soon!</div>;
+// const FeedPage = () => <div>Social Feed</div>;
 
 const App = () => {
   return (
-    <Routes>
+    <AuthProvider>
+      <Routes>
 
-      {/*  Routes without sidebar - after activate login authentication will open this route
-      <Route element={<AuthLayout />}>
-        <Route path="/signup" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Route>
-       */}
+        {/* Routes without sidebar - after activate login authentication will open this route */}
+        <Route element={<AuthLayout />}>
+          <Route path="/signup" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
 
-      {/*  Routes WITH sidebar */}
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/messages" element={<MessagePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/settings" element={<SettingPage />} />
-      </Route>
+        {/*  Routes WITH sidebar */}
 
-      {/* Catch-all */}
-      <Route path="*" element={<h1>404: Not Found</h1>} />
-    </Routes>
+        <Route
+          element={
+            <ProtectedRoute>
+              <MainLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/messages" element={<MessagePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/settings" element={<SettingPage />} />
+        </Route>
+
+        {/* <Route element={<MainLayout />}>
+        </Route> */}
+
+        {/* Catch-all */}
+        <Route path="*" element={<h1>404: Not Found</h1>} />
+      </Routes>
+
+    </AuthProvider>
   );
 };
 
