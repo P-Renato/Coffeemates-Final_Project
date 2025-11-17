8ea182b8118f06b6d29ded69b9a0127a12d0eb1b
Renato <paulo.borupdebakker@dci-student.org>
2025-11-17 14:25:14 +0100

    Updated Register page to have them hooked to backend API endpoint route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,10 +5,11 @@ import SearchPage from './features/search/SearchPage';
 import MessagePage from './features/messages/MessagePage';
 import ProfilePage from './features/profile/ProfilePage';
 import SettingPage from './features/settings/SettingPage';
+import OAuthSuccess from './pages/OAuthSuccess';
 
 
 const LoginPage = () => <div>Login Page Coming Soon!</div>;
-const FeedPage = () => <div>Social Feed</div>;
+// const FeedPage = () => <div>Social Feed</div>;
 
 const App = () => {
   return (
@@ -18,6 +19,8 @@ const App = () => {
         <Route path="/signup" element={<RegisterPage />} /> 
 
         <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/oauth-success" element={<OAuthSuccess />} />
 
         <Route path="/" element={<Home />} /> 
 
