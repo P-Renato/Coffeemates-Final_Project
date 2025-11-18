f89fd0e81896d4fd448b04257490d42e38c4eddd
Yukita0730 <yuki.haertinger@dci-student.org>
2025-11-18 10:19:51 +0100

    Plofie and sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,21 @@
 import Navbar from '../components/Navbar';
+import Settings from '../pages/Settings';
 import './index.css';
 
 export default function App() {
   const user = {
     name: 'Marie',
     photoURL: 'https://example.com/avatar.jpg',
-  }
+  };
 
   return (
     <div className="flex">
-      {/* ← left */}
       <Navbar user={user} />
 
-      {/* ← main */}
       <main className="flex-1 bg-[#f8f8f8] min-h-screen p-8">
-        <h1 className="text-2xl font-bold">Welcome to Coffeemates ☕️</h1>
+        <Settings />
       </main>
     </div>
-  )
+  );
 }
+
