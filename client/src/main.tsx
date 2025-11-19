6133c7965f7066a5624de14e882986be70edc9f5
Renato <paulo.borupdebakker@dci-student.org>
2025-11-19 21:29:12 +0100

    OAuth login working - Google - Facebook

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-//import 'leaflet/dist/leaflet.css';   // map
+import 'leaflet/dist/leaflet.css';   // map
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()],
-});
-=======
   plugins: [react(), tailwindcss(),],
   server: {
     port: 5173,
@@ -30,4 +26,3 @@ export default defineConfig({
     }
   }
 })
->>>>>>> c4a4d9d (OAuth login working - Google - Facebook)
