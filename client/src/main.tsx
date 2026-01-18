14eb05ff4cf609e58556b4623b9732f6ef24de2a
Renato <paulo.borupdebakker@dci-student.org>
2026-01-18 19:16:32 +0100

    debugging tests 2

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import 'leaflet/dist/leaflet.css';   // map
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* Use BrowserRouter to enable routing across the app */}
-    <BrowserRouter> 
       <App />
-    </BrowserRouter>
   </React.StrictMode>,
 );
